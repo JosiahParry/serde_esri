1025d95e7c660fa3e1d7b332584e5e4633e809a4
Josiah Parry <josiah.parry@gmail.com>
2024-04-16 15:56:20 -0400

    update places doc

--- src/places/query/mod.rs
+++ src/places/query/mod.rs
@@ -7,7 +7,14 @@
 //! The client is responsible for handling the authorization token and making the requests to the API.
 //!
 //! For [`PlacesClient::near_point()`] and [`PlacesClient::within_extent()`] queries, the client will automatically handle pagination and return an iterator over the results. This iterator will fetch the next page when needed. It will not, however, allow you to modify the page size. The default will be used.
-
+//!
+//! Each endpoint is supported by a query struct that contains the parameters for the query. These query structs each have a corresponding `Builder` ussed to build the request and prepare the parameters for the request.
+//!
+//! - [`PlaceQueryParamsBuilder`] creates a [`PlaceQueryParams`] struct which is used in the [`PlacesClient::place_details()`] method.
+//! - [`NearPointQueryParamsBuilder`] creates a [`NearPointQueryParams`] struct which is used in the [`PlacesClient::near_point()`] method.
+//! - [`WithinExtentQueryParamsBuilder`] creates a [`WithinExtentQueryParams`] struct which is used in the [`PlacesClient::within_extent()`] method.
+//! - [`CategoriesQueryParamsBuilder`] creates a [`CategoriesQueryParams`] struct which is used in the [`PlacesClient::categories()`] method.
+//! - [`CategoryQueryParamsBuilder`] creates a [`CategoryQueryParams`] struct which is used in the [`PlacesClient::category_details()`] method.
 mod query_params;
 mod responses;
 pub use query_params::*;
