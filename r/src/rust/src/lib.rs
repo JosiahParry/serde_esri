827cf52a2de70ad2308f180ddb2c27cc612d3dd2
Josiah Parry <josiah.parry@gmail.com>
2024-04-20 23:05:41 -0400

    get attributes processed

--- r/src/rust/src/lib.rs
+++ r/src/rust/src/lib.rs
@@ -137,55 +137,84 @@ fn as_polygon(x: List) -> String {
     }
 }
 
-use extendr_api::deserializer::from_robj;
-// use serde_esri::features::Feature;
-use serde_json::{Map, Value};
+use serde_json::{Map, Number, Value};
 
+// TODO if a column is a factor, make it a character factor
 #[extendr]
 pub fn as_attribute(x: List, n: i32) {
+    let n = n as usize;
     let ncol = x.len();
 
-    let i = 1_usize;
-
+    // extract columns from the data.frame
     let col_names = x
         .names()
         .unwrap()
         .map(|si| si.to_string())
         .collect::<Vec<_>>();
 
-    for i in 0..(n as usize) {
+    let mut feats = Vec::with_capacity(n);
+
+    for i in 0..(n) {
+        let mut map = Map::with_capacity(ncol);
         for j in 0..ncol {
-            let mut map = Map::with_capacity(ncol);
             let name = col_names[j].clone();
-            let col = x[j];
+            let col = &x[j];
             match col.rtype() {
                 Rtype::Doubles => {
                     let col_typed = Doubles::try_from(col).unwrap();
-                    let v = from_robj::<Value>(&col_typed[i].into());
-                    if let Ok(v) = v {
-                        map.insert(name, v);
-                    }
+                    let v = &col_typed[i];
+
+                    match !v.is_na() {
+                        true => {
+                            let num = Number::from_f64(v.inner())
+                                .expect("double can't be converted to serde_json::Number");
+                            map.insert(name, Value::Number(num))
+                        }
+                        false => map.insert(name, Value::Null),
+                    };
                 }
-                Rtype::Integers => todo!(),
-                Rtype::Strings => todo!(),
-                Rtype::Logicals => todo!(),
+                Rtype::Integers => {
+                    let col_typed = Integers::try_from(col).unwrap();
+                    let v = &col_typed[i];
+
+                    match !v.is_na() {
+                        true => {
+                            let num = Number::from_f64(v.inner() as f64)
+                                .expect("integer can't be converted to serde_json::Number");
+                            map.insert(name, Value::Number(num))
+                        }
+                        false => map.insert(name, Value::Null),
+                    };
+                }
+                Rtype::Strings => {
+                    let col_typed = Strings::try_from(col).unwrap();
+                    let rstr = &col_typed[i];
+                    match !rstr.is_na() {
+                        true => map.insert(name, Value::String(rstr.to_string())),
+                        false => map.insert(name, Value::Null),
+                    };
+                }
+                Rtype::Logicals => {
+                    let col_typed = Logicals::try_from(col).unwrap();
+                    let v = &col_typed[i];
+
+                    match !v.is_na() {
+                        true => map.insert(name, Value::Bool(v.to_bool())),
+                        false => map.insert(name, Value::Null),
+                    };
+                }
                 _ => unimplemented!(),
             }
         }
+
+        let feat = Feature::<2> {
+            attributes: Some(map),
+            geometry: None,
+        };
+        feats.push(feat);
     }
 
-    // for j in 0..ncol {
-    //     let name = col_names[j].clone();
-    //     let v = from_robj::<Value>(&x[j]);
-    //     map.insert(name, v.unwrap());
-    // }
-
-    // let feats = Feature::<0> {
-    //     geometry: None,
-    //     attributes: Some(map),
-    // };
-
-    // rprintln!("{:?}", feats);
+    rprintln!("{:#?}", feats);
 }
 
 // Macro to generate exports.
